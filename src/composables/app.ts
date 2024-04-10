959f33834773974cecc9df89d2b5f3a623133a89
pinky-pig <2545589993@qq.com>
2024-04-10 14:26:14 +0800

    fix: 更新 bento 依赖，改变首页默认排序

--- src/composables/app.ts
+++ src/composables/app.ts
@@ -26,6 +26,14 @@ export function toggleDark(event: MouseEvent) {
         `circle(0px at ${x}px ${y}px)`,
         `circle(${endRadius}px at ${x}px ${y}px)`,
       ]
+
+      // 也可以这样设置动画，直接将 keyframes 给 animate
+      // const keyframes = [
+      //   { clipPath: clipPath[0] }, // 初始状态
+      //   { clipPath: clipPath[1], offset: 0.5 }, // 停留在50%的时间
+      //   { clipPath: clipPath[2] } // 最终状态
+      // ];
+
       document.documentElement.animate(
         {
           clipPath: isDark.value
